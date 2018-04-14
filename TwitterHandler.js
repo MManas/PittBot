de45e232dd07c249fe5066c3cf02aa712eff3c65
Manas <mahajanm02@gmail.com>
2018-04-14 16:47:08 -0400

    created twitter feed handler

---
+++ TwitterHandler.js
@@ -0,0 +1,8 @@
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'x40PRLKMia1fuvctQPZxwmnmo',
+  consumer_secret: 'i3ETI9o5HmXS4bVzbkAIkq1zhpQMvCe7l9tYymkgtenNqv84AQ',
+  access_token_key: '949835403288293376-kG6Idg78FG9uK1rg8oTQFKeKx2IV6Dg',
+  access_token_secret: 'L5laUAyBgsGqt8zd2ww81WLK6uovws72IGZbJpIwcRMTw'
+});
